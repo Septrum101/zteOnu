299fa3c99679fa4e019172d95579fd5014c24815
Giammarco <stich86@gmail.com>
2024-10-09 16:55:43 +0200

    Update telnet.go
    Add firewall rule

--- app/telnet/telnet.go
+++ app/telnet/telnet.go
@@ -30,6 +30,10 @@ func (t *Telnet) PermTelnet() error {
 		return err
 	}
 
+	if err := t.modifyFW(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -44,12 +48,32 @@ func (t *Telnet) modifyDB() error {
 	tsLanUser := prefix + "TSLan_UName root"
 	tsLanPwd := prefix + "TSLan_UPwd Zte521"
 	maxConn := prefix + "Max_Con_Num 3"
-	initSecLvl := prefix + "InitSecLvl 3"
+	initSecLvl := prefix + "InitSecLvl 2"
 
 	// save DB
 	save := "sendcmd 1 DB save"
 
 	if err := t.sendCmd(lanEnable, tsLanUser, tsLanPwd, maxConn, initSecLvl, save); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Telnet) modifyFW() error {
+	// set DB data
+	prefix := "sendcmd 1 DB addr FWSC 0"
+	viewName := prefix + "ViewName IGD.FWSc.FWSC1"
+	enable := prefix + "Enable 1"
+	intName := prefix + "INCName LAN"
+	intViewName := prefix + "INCViewName IGD.LD1"
+	service := prefix + "Servise 8"
+	filter := prefix + "FilterTarget 1"
+
+	// save DB
+	save := "sendcmd 1 DB save"
+
+	if err := t.sendCmd(prefix, viewName, enable, intName, intViewName, service, filter, save); err != nil {
 		return err
 	}
 
